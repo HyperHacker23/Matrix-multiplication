c8fecf5a163d70f562cc097f433826d0c0237af2
Mukund Kannan <72849055+HyperHacker23@users.noreply.github.com>
2023-11-11 15:09:33 +0530

    Update Matrix_Multiplication.cpp

--- Matrix_Multiplication.cpp
+++ Matrix_Multiplication.cpp
@@ -23,37 +23,37 @@ if(col1==row2)
 	int b[row2][col2];
 	int c[col2][row1];
 //getting the values of elements of array 1
-	for(i=0;i<row1;i++)
+	for(i=0;i<row1;++i)
 	{
-		for(j=0;j<col1;j++)
+		for(j=0;j<col1;++j)
 		{
 			cout<<"Enter the value for element of row "<<i+1<<" and column "<<j+1<<endl;
 			cin>>a[i][j];
 		}
 	}
 //getting the values of elements of array 2
-	for(i=0;i<row2;i++)
+	for(i=0;i<row2;++i)
 	{
-		for(j=0;j<col2;j++)
+		for(j=0;j<col2;++j)
 		{
 			cout<<"Enter the value for element of row "<<i+1<<" and column "<<j+1<<endl;
 			cin>>b[i][j];
 		}
 	}
 //filling all elements of the resultant array as zero
-	for(i=0;i<col2;i++)
+	for(i=0;i<row1;++i)
 	{
-		for(j=0;j<row1;j++)
+		for(j=0;j<col2;++j)
 		{
 			c[i][j]=0;
 		}
 	}
 //calculating the value of the resultant array
-	for(i=0;i<col2;i++)
+	for(i=0;i<row1;++i)
 	{
-		for(j=0;j<row1;j++)
+		for(j=0;j<col2;++j)
 		{
-			for(k=0;k<col2;k++)
+			for(k=0;k<col1;++k)
 			{
 				c[i][j] += a[i][k]*b[k][j];
 				
@@ -63,9 +63,9 @@ if(col1==row2)
 	}
 //displaying the resultant array
 	cout<<"\nThe Resultant Matrix is: "<<endl;
-	for(i=0;i<col2;i++)
+	for(i=0;i<col2;++1)
 	{
-		for(j=0;j<row1;j++)
+		for(j=0;j<row1;++j)
 		{
 			cout<<c[i][j]<<"\t";
 		}
@@ -73,6 +73,6 @@ if(col1==row2)
 	}
 }
 else
-	cout<<"\nPlease learning basic arithmetics before using computers";
+	cout<<"\nPlease learn basic arithmetics before using computers";
 
 }
